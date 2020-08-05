18c52ea2138bc16a413ec5db8159abf75489dd1f
RannvijayKumar <rannvijaykr007@gmail.com>
2020-08-05 15:32:25 +0530

    Product Micro-service(First Commit)

---
+++ src/main/java/com/cg/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.cg.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.models.Product;
+import com.cg.repository.ProductRepository;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	ProductRepository repository;
+
+	@GetMapping(path = "/products")
+	public List<Product> retriveAllProducts() {
+		System.out.println("Inside retriveAllProducts() of Controller");
+		return repository.findAll();
+	}
+
+	@PostMapping(path = "/add")
+	public void add(@Valid @RequestBody Product product) {
+		System.out.println("Inside add() of Controller");
+		repository.save(product);
+	}
+
+	@DeleteMapping(path = "/delete/{id}")
+	public List<Product> delete(@PathVariable int id) {
+		System.out.println("Inside delete() of Controller");
+		repository.deleteById(id);
+		return repository.findAll();
+	}
+
+}
